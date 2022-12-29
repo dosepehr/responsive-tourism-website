7b4aeadd249b512d4463ee7e8bbf1a1442349a30
dosepehr <dosepehr@gmail.com>
2022-12-29 19:48:30 +0330

    refactor js code

--- js/index.js
+++ js/index.js
@@ -5,25 +5,21 @@ const menu = $.querySelector('.menu');
 const closer = $.querySelector('.menu-closer');
 const overlay = $.querySelector('.overlay2')
 
-opener.addEventListener('click', function () {
-    opener.classList.toggle('active');
-    menu.classList.toggle('show');
-    closer.classList.toggle('close');
-    overlay.classList.toggle('active')
-});
-
-
-closer.addEventListener('click', function () {
-    opener.classList.toggle('active');
-    menu.classList.toggle('show');
-    closer.classList.toggle('close');
-    overlay.classList.toggle('active')
-});
-
-
-overlay.onclick = () => {
+const menuToggler = () => {
     opener.classList.toggle('active');
     menu.classList.toggle('show');
     closer.classList.toggle('close');
     overlay.classList.toggle('active')
 }
+
+opener.onclick = () => {
+    menuToggler()
+}
+closer.onclick = () => {
+    menuToggler()
+}
+
+overlay.onclick = () => {
+    menuToggler()
+}
+
