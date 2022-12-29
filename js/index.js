c75cbd99012c5364e60a4939c6e4a0dc44c8f825
dosepehr <dosepehr@gmail.com>
2022-12-29 19:45:39 +0330

    add overlay to dom

--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
-
-
-
-
 var $ = document;
 
 const opener = $.querySelector('.menu-opener');
 const menu = $.querySelector('.menu');
 const closer = $.querySelector('.menu-closer');
+const overlay = $.querySelector('.overlay2')
+
 opener.addEventListener('click', function () {
     opener.classList.toggle('active');
     menu.classList.toggle('show');
     closer.classList.toggle('close');
+    overlay.classList.toggle('active')
 });
+
+
 closer.addEventListener('click', function () {
     opener.classList.toggle('active');
     menu.classList.toggle('show');
     closer.classList.toggle('close');
+    overlay.classList.toggle('active')
 });
+
+
+overlay.onclick = () => {
+    opener.classList.toggle('active');
+    menu.classList.toggle('show');
+    closer.classList.toggle('close');
+    overlay.classList.toggle('active')
+}
